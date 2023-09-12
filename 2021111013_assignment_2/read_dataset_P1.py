a49c943212de40d42a52459b02afa668d6968ce5
Bhav Beri <bhavberi@gmail.com>
2023-09-13 01:18:50 +0530

    Added P1 Notebook (Largely incomplete)

---
+++ 2021111013_assignment_2/read_dataset_P1.py
@@ -0,0 +1,76 @@
+import numpy as np
+import os
+from random import randrange
+from matplotlib import pyplot as plt
+from PIL import Image
+
+cfw_dict = {
+    'Amitabhbachan': 0,
+    'AamirKhan': 1,
+    'DwayneJohnson': 2,
+    'AishwaryaRai': 3,
+    'BarackObama': 4,
+    'NarendraModi': 5,
+    'ManmohanSingh': 6,
+    'VladimirPutin': 7
+}
+
+opt = {
+    'image_size': 32,
+    'is_grayscale': False,
+    'val_split': 0.75
+}
+
+def load_image(path):
+    im = Image.open(path).convert('L' if opt['is_grayscale'] else 'RGB')
+    im = im.resize((opt['image_size'],opt['image_size']))
+    im = np.array(im)
+    im = im/256
+    return im
+
+def display_images(imgs,classes,row=1,col=2,w=32,h=32):
+    fig=plt.figure(figsize=(8, 8))
+    for i in range(1, col*row +1):
+        img = imgs[i-1]
+        fig.add_subplot(row, col, i)
+        
+        if opt['is_grayscale']:
+            plt.imshow(img , cmap='gray') 
+        else:
+            plt.imshow(img)
+        
+        plt.title("Class:{}".format(classes[i-1]))
+        plt.axis('off')
+    plt.show()
+
+def load_data(dir_path):
+    image_list = []
+    y_list = []
+    label_dict = cfw_dict
+    for filename in sorted(os.listdir(dir_path)):
+        if filename.endswith(".png"):
+            im = load_image(os.path.join(dir_path,filename))
+            y = filename.split('_')[0]
+            y = label_dict[y] 
+            image_list.append(im)
+            y_list.append(y)
+        else:
+            continue
+    image_list = np.array(image_list)
+    y_list = np.array(y_list)
+    print("Dataset shape:",image_list.shape)
+    print("Label shape:",y_list.shape)
+
+    return image_list,y_list
+
+def main():
+    dirpath = '../SMAI-PCAdataset/IIIT-CFW/'
+    X,y = load_data(dirpath)
+
+    N,H,W = X.shape[0:3]
+    C = 1 if opt['is_grayscale'] else X.shape[3]
+
+    ind = np.random.randint(0,y.shape[0],6)
+    display_images(X[ind,...],y[ind], row=2,col=3)
+
+    return X,y
